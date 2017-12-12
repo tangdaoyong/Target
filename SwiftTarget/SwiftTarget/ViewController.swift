31f419495025d450db442ab8fcf03a47615cdf0b
唐道勇 <tangdaoyong@tangdaoyongdeMacBook-Pro-2.local>
2017-12-12 22:21:16 +0800

    添加了github链接地址

--- SwiftTarget/SwiftTarget/ViewController.swift
+++ SwiftTarget/SwiftTarget/ViewController.swift
@@ -14,6 +14,7 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         /*
+         https://github.com/tangdaoyong/Target
          build Settings -> Other Swift Flags ->Debug
          添加SWIFTTWO宏定义：
          $(inherited)
